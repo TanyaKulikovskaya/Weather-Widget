534a6d1f6b573071fad1ab5cef26aeda17905d45
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2023-02-07 11:37:05 +0300

    display location list

---
+++ src/components/Settings/LocationList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li
+      v-for="(item, index) in locations"
+      :key="index"
+      class="location-list__item"
+    >
+      {{ item.city }}, {{ item.country }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "LocationList",
+  computed: {
+    ...mapGetters(["locations"]),
+  },
+};
+</script>
+
+<style lang="scss">
+.location-list__item {
+  padding: 6px 8px;
+  background-color: #dee2e6;
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+}
+</style>
--- src/components/Settings/WidgetSettings.vue
+++ src/components/Settings/WidgetSettings.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="settings">
     <p class="settings__title">Settings</p>
+    <location-list class="settings__locations" />
     <add-location />
   </div>
 </template>
 
 <script>
 import AddLocation from "./AddLocation.vue";
+import LocationList from "./LocationList.vue";
 
 export default {
   name: "WidgetSettings",
   components: {
     AddLocation,
+    LocationList,
   },
 };
 </script>
@@ -21,6 +24,9 @@ export default {
   min-width: 260px;
   &__title {
     line-height: 36px;
+  }
+  &__locations {
+    margin-bottom: 18px;
   }
 }
 </style>
